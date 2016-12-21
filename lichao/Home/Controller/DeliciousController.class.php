c7eb0960a0e295185941442486da1bb672b2e0f5
Dreamer <1920428758@qq.com>
2016-12-21 16:57:51 +0800

    李超

--- lichao/Home/Controller/DeliciousController.class.php
+++ lichao/Home/Controller/DeliciousController.class.php
@@ -3,39 +3,74 @@ namespace Home\Controller;
 use Think\Controller;
 class DeliciousController extends Controller {
 	// 小吃
-    public function snacks(){
-        $sliModel = M("sliders");
-    	$model = M("sellertab");
-        $actModel = M("activity");
-        $condition = array(
-                "type_origin" => '美食',
-                "type_branch" => '小吃'
-            );
-        $data = array(
-            "type_branch"=>'小吃'
-        );
-        $sliders=$sliModel->where($data)->select();
-        $activity=$actModel->where($data)->select();
-    	$seller = $model->where($condition)->select();
-        $this->assign("sliders",$sliders);
-    	$this->assign("sellertab", $seller);
-        $this->assign("activity",$activity);
-// 小徽章
-        $data['busername']=$_SESSION['user']['busername'];
-        //print_r($data);exit;
-        $this->assign('user',$data);
+   public function snacks(){
+           $sliModel = M("sliders");
+           $actModel = M("activity");
+           $shopModel=M("sellertab");
+           $condition = array(
+                   "type_origin" => '美食',
+                   "type_branch" => '小吃'
+               );
+           $data = array(
+               "type_branch"=>'小吃'
+           );
+
+           $sliders=$sliModel->where($data)->select();
+           $activity=$actModel->where($data)->select();
+       	$shop= $shopModel->where($condition)->select();
+           $this->assign("sliders",$sliders);
+           $this->assign("activity",$activity);
+   // print_r($activity);exit;
+           $data['busername']=$_SESSION['user']['busername'];
+           $this->assign('user',$data);
 
 
-        $chipsModel = M("chips");
-        $condition = array(
-                "type_branch" => '小吃'
-            );
-        $chips = $chipsModel->where($condition)->select();
-        $this->assign('chips', $chips);
+           $chipsModel=M("chips");
+           $sid=$shopModel->where("type_branch='小吃'")->field('sid')->select();
+           foreach ($sid as $key => $value) {
+                $sid[$key]=$sid[$key]['sid'];
+            }//sid的数组
+           $count=$chipsModel->where("id")->field('clickcount')->select();
+           $male=$chipsModel->where("id")->field('malenum')->select();
+           $female=$chipsModel->where("id")->field('femalenum')->select();
+           $length=$chipsModel->count();
+           for($i=0;$i<$length;$i++)
+           {
+               $malenum[$i]=round($male[$i]['malenum']/$count[$i]['clickcount'],2)*100;
+               $femalenum[$i]=round($female[$i]['femalenum']/$count[$i]['clickcount'],2)*100;
+           }
+           foreach($malenum as $key=>$val)
+           {
+               $data['malewidth']=$val;
+               $data11=array(
+                   "id"=>$key+1
+                   );
+               $chipsModel->where($data11)->field('malewidth')->save($data);
+           }
+           foreach($femalenum as $key=>$val)
+           {
+               $data['femalewidth']=$val;
+               $data11=array(
+                   "id"=>$key+1
+                   );
+               $chipsModel->where($data11)->field('femalewidth')->save($data);
+           }
+           $progress=$chipsModel->where("type_branch='小吃'")->order('clickcount+0 desc')->limit(0,2)->select();
+           foreach($sid as $key => $value) {
+               $chips[$key] = $chipsModel->where("sid='$value'")->field('chipname,clickcount,id,femalenum,malenum,malewidth,femalewidth')->order('clickcount+0 desc')->select();
+           }//0店  0 1 2。。的标签
 
-
-    	$this->display();
-    }
+            foreach($chips as $key=>$value){
+              foreach ($value as $k => $v) {
+                  $data[$key][$k]=$v;
+              }
+             }
+           foreach($shop as $key=>$val){
+                $shop[$key]['chiptwo']=$data[$key];
+            }
+           $this->assign('sellertab', $shop);
+       	$this->display();
+       }
 	// 甜品
     public function dessert(){
         $sliModel = M("sliders");
